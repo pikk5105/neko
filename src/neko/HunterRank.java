77f3f3995836aebb626bb1b9cad32115a73df804
Dominik Koellges <dominik.koellges@gmail.com>
2016-07-21 04:16:22 +0200

    Hunt and HunterRank classes added
    Created HunterRank for autonomous low/high rank differentiation and
    added scalability by creating the Hunt class

---
+++ src/neko/HunterRank.java
@@ -0,0 +1,31 @@
+public class HunterRank
+{
+  private int rank = 0;
+
+  HunterRank(int newRank)
+  {
+    SetRank(newRank);
+  }
+
+  public void SetRank(int newRank)
+  {
+    rank = newRank;
+  }
+
+  public int GetRank()
+  {
+    return rank;
+  }
+
+  public String GetRankType()
+  {
+    if(rank <= 4)
+    {
+      return "Low";
+    }
+    else
+    {
+      return "High";
+    }
+  }
+}
