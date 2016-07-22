582dce204b52ea30330b85c9f23b7747eebef66c
Dominik Koellges <dominik.koellges@gmail.com>
2016-07-23 01:13:36 +0200

    Filter and FilterHandler added, the former having all the filters and their logic, the latter handing interaction with the filters.

---
+++ src/neko/FilterHandler.java
@@ -0,0 +1,9 @@
+package neko;
+
+/**
+ * Created by Dominik on 23.07.2016.
+ */
+public class FilterHandler
+{
+    //  TODO: This class will handle filter requests
+}
---
+++ src/neko/Filters.java
@@ -0,0 +1,9 @@
+package neko;
+
+/**
+ * Created by Dominik on 21.07.2016.
+ */
+public class Filters
+{
+
+}
