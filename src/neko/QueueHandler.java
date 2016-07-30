47187df4ff32d0fc1f8cd649a621b8b2b3e9c46b
Dominik Koellges <dominik.koellges@gmail.com>
2016-07-30 05:29:38 +0200

    Drafted QueueHandler

--- src/neko/QueueHandler.java
+++ src/neko/QueueHandler.java
@@ -1,16 +1,35 @@
 package neko;
 
+import java.util.*;
+
 /**
  * Created by Dominik on 21.07.2016.
  */
 public class QueueHandler
 {
-    Queue queue;
+    private HashMap<String, Queue> queues;
 
     public QueueHandler()
+    {
+        queues = new HashMap<String, Queue>();
+    }
+
+    public Queue GetQueue(String queueName)
+    {
+        return queues.get(queueName);
+    }
+
+    public void AddQueueObject(String key, Queue queue)
+    {
+        queues.put(key, queue);
+    }
+
+    public void RemoveQueueObject(Queue queue)
     {
 
     }
 
     //  TODO: Add Queue Manipulation Logic
+
+
 }
