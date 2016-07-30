b6b25f116b575b663b2fd7df30e33bcbb5cb4c9a
Dominik Koellges <dominik.koellges@gmail.com>
2016-07-30 19:29:01 +0200

    added ProfilerImporter.java as an abstraction layer between Neko maincode and data storage.

---
+++ src/neko/DataManagement/ProfileImporter.java
@@ -0,0 +1,9 @@
+package neko.DataManagement;
+
+/**
+ * Created by Dominik on 30.07.2016.
+ */
+public class ProfileImporter
+{
+    //TODO: Seperate Profile Importing solely to this class, so there is a single point of failure possible as well as serving as an abstraction layer
+}
