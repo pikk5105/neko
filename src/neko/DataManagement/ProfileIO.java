1577fe1dad6c6d4cc80de0ae9a4cca15af2f1b1f
Dominik Koellges <dominik.koellges@gmail.com>
2016-08-04 23:13:22 +0200

    Renamed ProfileIO class to properly reflect name change

--- src/neko/DataManagement/ProfileIO.java
+++ src/neko/DataManagement/ProfileIO.java
@@ -3,7 +3,7 @@ package neko.DataManagement;
 /**
  * Created by Dominik on 30.07.2016.
  */
-public class ProfileImporter
+public class ProfileIO
 {
     //TODO: Seperate Profile Importing solely to this class, so there is a single point of failure possible as well as serving as an abstraction layer
 }
